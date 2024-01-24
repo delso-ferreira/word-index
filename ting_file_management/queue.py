0ccac7b392854f1d863065b7d36efef40b5c8688
delso-ferreira <delsofelipe@gmail.com>
2024-01-24 15:02:38 -0300

    Add Question 1

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,18 +1,51 @@
 from ting_file_management.abstract_queue import AbstractQueue
 
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.first = None
+        self.last = None
+        self._size = 0
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return self._size
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        node = Node(value)
+        if self.last is None:
+            self.last = node
+        else:
+            self.last.next = node
+            self.last = node
+
+        if self.first is None:
+            self.first = node
+        
+        self._size += 1
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if self._size == 0:
+            raise Exception("Fila vazia")
+        
+        value = self.first.value
+        self.first = self.first.next
+
+        if self.first is None:
+            self.last = None
+
+        self._size -= 1
+        return value
 
     def search(self, index):
-        """Aqui irá sua implementação"""
+        if index < 0 or index >= self._size:
+            raise IndexError("Índice Inválido ou Inexistente")
+        
+        pointer = self.first
+        for _ in range(index):
+            pointer = pointer.next
+        return pointer.value
