3ec04b405c480fe5063843deec9148dc05ebd8d8
delso-ferreira <delsofelipe@gmail.com>
2024-01-24 17:24:00 -0300

    add Question 2

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,20 @@
-def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+import os
+import sys
+
+
+def txt_importer(filename):
+    if not os.path.isfile(filename):
+        sys.stderr.write(f"Arquivo {filename} não encontrado\n")
+        return
+
+    _, extension = os.path.splitext(filename)
+    if extension != ".txt":
+        sys.stderr.write("Formato inválido\n")
+        return
+
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+
+    lines = [line.rstrip('\n') for line in lines]
+
+    return lines
