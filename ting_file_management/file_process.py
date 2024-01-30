e9039919d0e02faf7f4200f67f7ea8a5a5f9d218
delso-ferreira <delsofelipe@gmail.com>
2024-01-29 21:28:36 -0300

    WIP Requisito 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+def process(path_file, instance: Queue):    
+    for i in instance.enqueue(path_file):
+        if i['nome_do_arquivo'] == path_file:
+            return None
+    
+    text = txt_importer(path_file)
+    data = {
+        'nome_do_arquivo': path_file,
+        'qtd_linhas': len(text),
+        'linhas_do_arquivo': text,
+        }
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
