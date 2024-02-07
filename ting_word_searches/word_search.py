7410ecd8456fffe354ee5514b4b788e07171e3c4
delso-ferreira <delsofelipe@gmail.com>
2024-02-07 11:09:03 -0300

    Update req 07

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,27 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+
+    results = []
+
+    current_node = instance.first
+    while current_node is not None:
+        file = current_node.value
+        occurrences = []
+
+        for i, line in enumerate(file['linhas_do_arquivo']):
+            if word.lower() in line.lower():
+                occurrences.append({"linha": i + 1})
+
+        if occurrences:
+            results.append({
+                "palavra": word,
+                "arquivo": file['nome_do_arquivo'],
+                "ocorrencias": occurrences
+            })
+
+        current_node = current_node.next
+
+    return results
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    pass
