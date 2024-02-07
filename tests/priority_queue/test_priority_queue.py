0beaab4c2261df7eb1e777d78c7c169bdafff833
delso-ferreira <delsofelipe@gmail.com>
2024-02-07 10:31:15 -0300

    Update Req 06

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -2,88 +2,19 @@ import pytest
 from ting_file_management.priority_queue import PriorityQueue
 
 
-@pytest.fixture
-def priority_queue():
-    return PriorityQueue()
+def test_basic_priority_queueing():
+    priority_queue = PriorityQueue()
 
+    priority_queue.enqueue({"qtd_linhas": 4})
+    priority_queue.enqueue({"qtd_linhas": 2})
 
-def test_enqueue_priority(priority_queue):
-    priority_queue.enqueue({
-        'nome_do_arquivo': 'file1.txt',
-        'qtd_linhas': 4,
-        'linhas_do_arquivo': ['line1', 'line2', 'line3', 'line4']
-    })
-    priority_queue.enqueue({
-        'nome_do_arquivo': 'file2.txt',
-        'qtd_linhas': 6,
-        'linhas_do_arquivo': [
-            'line1', 'line2', 'line3', 'line4', 'line5', 'line6'
-        ]
-    })
     assert len(priority_queue) == 2
 
+    assert priority_queue.search(0) == {"qtd_linhas": 2}
+    assert priority_queue.search(1) == {"qtd_linhas": 4}
 
-def test_dequeue_priority(priority_queue):
-    priority_queue.enqueue({
-        'nome_do_arquivo': 'file1.txt',
-        'qtd_linhas': 4,
-        'linhas_do_arquivo': ['line1', 'line2', 'line3', 'line4']
-    })
-    priority_queue.enqueue({
-        'nome_do_arquivo': 'file2.txt',
-        'qtd_linhas': 6,
-        'linhas_do_arquivo': [
-            'line1', 'line2', 'line3', 'line4', 'line5', 'line6'
-        ]
-    })
-    assert priority_queue.dequeue()['nome_do_arquivo'] == 'file1.txt'
+    assert priority_queue.dequeue() == {"qtd_linhas": 2}
+    assert len(priority_queue) == 1
 
-
-def test_dequeue_regular(priority_queue):
-    priority_queue.enqueue({
-        'nome_do_arquivo': 'file1.txt',
-        'qtd_linhas': 4,
-        'linhas_do_arquivo': ['line1', 'line2', 'line3', 'line4']
-    })
-    priority_queue.enqueue({
-        'nome_do_arquivo': 'file2.txt',
-        'qtd_linhas': 6,
-        'linhas_do_arquivo': [
-            'line1', 'line2', 'line3', 'line4', 'line5', 'line6'
-        ]
-    })
-
-    assert priority_queue.dequeue()['nome_do_arquivo'] == 'file1.txt'
-    assert priority_queue.dequeue()['nome_do_arquivo'] == 'file2.txt'
-
-
-def test_search_priority(priority_queue):
-    priority_queue.enqueue({
-        'nome_do_arquivo': 'file1.txt',
-        'qtd_linhas': 4,
-        'linhas_do_arquivo': ['line1', 'line2', 'line3', 'line4']
-    })
-    priority_queue.enqueue({
-        'nome_do_arquivo': 'file2.txt',
-        'qtd_linhas': 6,
-        'linhas_do_arquivo': [
-            'line1', 'line2', 'line3', 'line4', 'line5', 'line6'
-        ]
-    })
-    assert priority_queue.search(0)['nome_do_arquivo'] == 'file1.txt'
-
-
-def test_search_regular(priority_queue):
-    priority_queue.enqueue({
-        'nome_do_arquivo': 'file1.txt',
-        'qtd_linhas': 4,
-        'linhas_do_arquivo': ['line1', 'line2', 'line3', 'line4']
-    })
-    priority_queue.enqueue({
-        'nome_do_arquivo': 'file2.txt',
-        'qtd_linhas': 6,
-        'linhas_do_arquivo': [
-            'line1', 'line2', 'line3', 'line4', 'line5', 'line6'
-        ]
-    })
-    assert priority_queue.search(1)['nome_do_arquivo'] == 'file2.txt'
+    with pytest.raises(IndexError):
+        priority_queue.search(1)
